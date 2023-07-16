78dc21fe31dec8874c354ef7dea3f435a2735d62
govindkrishna03 <govindkrishna28@gmail.com>
2023-07-16 10:33:41 +0530

    contract

--- my-app/src/pages/donate.js
+++ my-app/src/pages/donate.js
@@ -1,18 +1,17 @@
 import MainNavigation from "./main-navigation";
-import { useState, useContext } from "react";
+import { useState } from "react";
 import Link from "next/link";
 import AuthenticationContext from "@/context/AuthenticationContext";
 import Footer from "./footer";
 import axios from "axios";
-import { InputTwoTone } from "@mui/icons-material";
 
 const DonatePage = () => {
   const [name, setName] = useState("");
-  const [Address, setAge] = useState("");
+  const [address, setAddress] = useState("");
   const [email, setEmail] = useState("");
-  const [phone, setProblem] = useState("");
-  const [PinCode, setAmount] = useState("");
-  const [machine, setUpi] = useState("");
+  const [phone, setPhone] = useState("");
+  const [pinCode, setPinCode] = useState("");
+  const [machine, setMachine] = useState("");
   const [quantity, setQuantity] = useState("");
 
   const handleSubmit = async (e) => {
@@ -20,10 +19,10 @@ const DonatePage = () => {
 
     const data = {
       name,
-      Address,
+      address,
       email,
       phone,
-      PinCode,
+      pinCode,
       machine,
       quantity,
     };
@@ -50,18 +49,17 @@ const DonatePage = () => {
         </div>
         <div className="border border-black p-5">
           <div className="relative flex flex-col justify-center min-h-screen overflow-hidden bg-white">
-          <div className="w-full p-20 m-auto bg-white border border-black bg-opacity-90 rounded-md lg:max-w-xl">
-  <h2 className="text-3xl font-semibold text-center text-blue-600 uppercase">
+            <div className="w-full p-20 m-auto bg-white border border-black bg-opacity-90 rounded-md lg:max-w-xl">
+              <h2 className="text-3xl font-semibold text-center text-blue-600 uppercase">
                 Upload Your Details Here
               </h2>
-              <form className="mt-6 " onSubmit={handleSubmit}>
+              <form className="mt-6" onSubmit={handleSubmit}>
                 <div className="mb-2 border">
                   <label className="">Name:</label>
                   <input
                     className="block w-full h-[40px] px-4 py-2 mt-2 text-gray-800 bg-white border border-black rounded-md focus:border-black-600 focus:ring focus:ring-opacity-40"
                     type="text"
                     value={name}
-                    name="name"
                     onChange={(event) => setName(event.target.value)}
                   />
                 </div>
@@ -70,9 +68,8 @@ const DonatePage = () => {
                   <input
                     className="block w-full h-[80px] px-4 py-2 mt-2 text-gray-800 bg-white border border-black rounded-md focus:border-black-600 focus:ring focus:ring-opacity-40"
                     type="text"
-                    value={Address}
-                    name="age"
-                    onChange={(event) => setAge(event.target.value)}
+                    value={address}
+                    onChange={(event) => setAddress(event.target.value)}
                   />
                 </div>
                 <div className="">
@@ -81,7 +78,6 @@ const DonatePage = () => {
                     className="block w-full h-[30px] px-4 py-2 mt-2 text-gray-800 bg-white border border-black rounded-md focus:border-black-600 focus:ring focus:ring-opacity-40"
                     type="email"
                     value={email}
-                    name="email"
                     onChange={(event) => setEmail(event.target.value)}
                   />
                 </div>
@@ -91,8 +87,7 @@ const DonatePage = () => {
                     className="block w-full h-[30px] px-4 py-2 mt-2 text-gray-800 bg-white border border-black rounded-md focus:border-black-600 focus:ring focus:ring-opacity-40"
                     type="text"
                     value={phone}
-                    name="problem"
-                    onChange={(event) => setProblem(event.target.value)}
+                    onChange={(event) => setPhone(event.target.value)}
                   />
                 </div>
                 <div className="">
@@ -100,9 +95,8 @@ const DonatePage = () => {
                   <input
                     className="block w-full h-[30px] px-4 py-2 mt-2 text-gray-800 bg-white border border-black rounded-md focus:border-black-600 focus:ring focus:ring-opacity-40"
                     type="text"
-                    value={PinCode}
-                    name="amount"
-                    onChange={(event) => setAmount(event.target.value)}
+                    value={pinCode}
+                    onChange={(event) => setPinCode(event.target.value)}
                   />
                 </div>
                 <div className="">
@@ -111,8 +105,7 @@ const DonatePage = () => {
                     className="block w-full h-[30px] px-4 py-2 mt-2 text-gray-800 bg-white border border-black rounded-md focus:border-black-600 focus:ring focus:ring-opacity-40"
                     type="text"
                     value={machine}
-                    name="upi"
-                    onChange={(event) => setUpi(event.target.value)}
+                    onChange={(event) => setMachine(event.target.value)}
                   />
                 </div>
                 <div className="">
@@ -136,17 +129,15 @@ const DonatePage = () => {
                     type="number"
                     min="0"
                     value={quantity}
-                    name="quantity"
                     onChange={(event) => setQuantity(event.target.value)}
                   />
                 </div>
                 <div className="flex justify-center items-center mt-5">
-                  <button
-                    className="hover:bg-green-500 bg-red-500 text-white py-2 px-4 rounded"
-                    type="submit"
-                  >
-                    <Link href="/payment">Submit</Link>
-                  </button>
+                  <Link href="/payment">
+                    <button className="hover:bg-green-500 bg-red-500 text-white py-2 px-4 rounded" type="submit">
+                      Submit
+                    </button>
+                  </Link>
                 </div>
               </form>
             </div>
